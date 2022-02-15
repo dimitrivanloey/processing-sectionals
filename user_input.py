0076a96769db42c97884211893010aaeb8608f80
Dimitri Van Loey <dimitrivanloey@gmail.com>
2022-02-15 16:06:15 +0000

    second commit

--- user_input.py
+++ user_input.py
@@ -1,11 +1,16 @@
+# Example 'kempton-park' - 'catterick-bridge' - 'exeter'
 VENUE = 'catterick-bridge'
 
+# Always format as follows YYYYMMDD - Example '20220214'
 DATE = '20220214'
 
+# Exact name of the excel sheet - Example f'CAT_1302.xlsx'
 FILE = f'CAT_1302.xlsx'
 
+# List of the hours in the right order - Example ['1300', '1330', '1405', '1440', '1515', '1550']
 HOURS_LIST = ['1300', '1330', '1405', '1440', '1515', '1550']
 
+# List of the title is the right order
 TITLES_LIST = ["2m 3f 66y Love Racing Handicap Hurdle",
                "2m 3f 51y Jumping For Joy On Racing TV Novices' Handicap Chase (GBB Race)",
                "3m 1f 71y Meetings That Matter On Racing TV Mares' Novices' Hurdle (GBB Race)",
@@ -14,3 +19,5 @@ TITLES_LIST = ["2m 3f 66y Love Racing Handicap Hurdle",
                "1m 7f 156y Racing Again 1st March Open NH Flat Race (GBB Race)"]
 
 
+
+# Dependencies pip install requests numpy openpyxl pandas pdfkit
