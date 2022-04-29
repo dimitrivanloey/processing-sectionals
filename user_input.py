5d71b97b4cdf040f6726b38c758e5b8f17b78298
Dimitri Van Loey <dimitrivanloey@gmail.com>
2022-04-29 13:36:28 +0100

    added requirements.txt file

--- user_input.py
+++ user_input.py
@@ -1,23 +1,18 @@
 # Examples 'kempton-park' - 'catterick-bridge' - 'exeter' - 'sandown-park' - 'haydock-park' - 'stratford-on-avon'
-VENUE = 'musselburgh'
+VENUE = 'redcar'
 
 # Always format as follows YYYYMMDD - Example '20220214'
-DATE = '20220416'
+DATE = '20220428'
 
 # Name of the excel sheet - Example f'CAT_1302.xlsx'
-FILE = f'MUS_1604.xlsx'
+FILE = f'RED_2804.xlsx'
 
 # List of the hours in the right order - Example ['1300', '1330', '1405', '1440', '1515', '1550']
-HOURS_LIST = ['1332', '1404', '1439', '1514', '1547', '1622', '1657']
+HOURS_LIST = ['1340', '1410', '1440', '1515', '1550', '1625']
 
 # List of the titles in the right order (between double quotes and seperated by commas)
-TITLES_LIST = ["1m 0f 2y Betway Royal Mile Handicap",
-"0m 7f 33y Betway Musselburgh Silver Arrow Handicap",
-"0m 5f 1y Betway Holyrood Handicap",
-"1m 5f 216y Betway Queen's Cup (Heritage Handicap)",
-"0m 5f 1y Betway EBF Novice Stakes (GBB Race)",
-"0m 5f 1y Read Andrew Balding On Betway Insider Handicap",
-"1m 0f 2y Betway Handicap"]
+TITLES_LIST = ["1m 2f 1y Racing TV Club Day Here Today Claiming Stakes", "1m 2f 1y Racing TV Profits Returned To Racing Handicap", "0m 5f 0y Join Racing TV Half Price Now Selling Stakes", "0m 5f 217y Every Race Live On Racing TV Fillies' Novice Stakes (GBB Race)", "0m 7f 0y Join Racing TV For Half Price Restricted Novice Stakes", "0m 7f 219y Watch Racing TV In Stunning HD Handicap"]
 
 
 # Dependencies pip install requests numpy openpyxl pandas pdfkit Pillow wkhtmltopdf PyPDF2
+
